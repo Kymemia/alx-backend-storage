83bef5d7fe76a39f047e5a3495bf3d464a7b57cc
Kymemia <kimemiachristopherr@gmail.com>
2024-08-11 02:37:24 +0300

    comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- creates a table, users, with the following requirements:
+-- -> id (int, never null, autoincrement & primary key)
+-- -> email (string, never null & unique)
+-- -> name (string)
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY AUTOINCREMEMNT,
 	email VARCHAR(255) NOT NULL UNIQUE,

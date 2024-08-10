d5d18ad8a13db820356edd801e50c285d6262b4e
Kymemia <kimemiachristopherr@gmail.com>
2024-08-11 02:47:28 +0300

    unique fix => auto_increment issue fixed

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 -- -> email (string, never null & unique)
 -- -> name (string)
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMEMNT,
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 	);

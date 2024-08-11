203a5d4c11e3778212cbca565e35a531ea41184f
Kymemia <kimemiachristopherr@gmail.com>
2024-08-11 03:21:52 +0300

    task 3. run 2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,7 @@
 -- this script lists all bands with <Glam rock> as their main style, ranked by their longevity
 
-SELECT band_name (2022 - formed) - COALESCE(split, 2022) AS lifespan FROM bands WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT 
+	band_name,
+       	COALESCE(split, 2022) AS lifespan FROM bands
+	WHERE style = 'Glam rock'
+	ORDER BY lifespan DESC;
